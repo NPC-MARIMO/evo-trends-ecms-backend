0c52542f229d4df314e1a59e26f7c578c841e811
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2024-12-06 00:32:06 +0530

    correcting till it works

--- server.js
+++ server.js
@@ -27,8 +27,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  credentials: true,              
+  origin: 'https://evo-trends-ecms.vercel.app', // Specific frontend URL
+  credentials: true // Allow cookies, authorization headers, etc.
 }));
+
+app.options('*', cors({
+  origin: 'https://evo-trends-ecms.vercel.app',
+  credentials: true
+}));
+
 
 app.use(cookieParser());
 app.use(express.json());
