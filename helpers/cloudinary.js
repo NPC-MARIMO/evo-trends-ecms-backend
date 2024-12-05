05180e05b5e5f755192a70712912684a9d9df110
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2024-12-06 01:42:38 +0530

    console.log

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -3,19 +3,17 @@ const multer = require("multer");
 require('dotenv').config();
 
 cloudinary.config({
-  cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
-  api_key : process.env.CLOUDINARY_API_KEY,
-  api_secret : process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new multer.memoryStorage();
 
 async function imageUploadUtil(file) {
-  const result = await cloudinary.uploader.upload(file.tempFilePath, {
+  return await cloudinary.uploader.upload(file.tempFilePath, {
     resource_type:"auto",
 })
-  console.log(result)
-  return result;
 }
 
 const upload = multer({ storage });
