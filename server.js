f8bfa80a4055dca866ff007b5c20917bb2091660
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2024-12-05 23:42:51 +0530

    checkauth

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // specify your frontend origin
+  credentials: true,              // allow cookies and credentials
+}));
 
 app.use(cookieParser());
 app.use(express.json());
