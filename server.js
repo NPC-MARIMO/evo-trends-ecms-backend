9686d337b7db6f9b89ac0411f224746d2bcd4b0d
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2024-12-05 22:48:13 +0530

    db-err

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("db_url")
+  .connect(process.env.MONGO_DB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
